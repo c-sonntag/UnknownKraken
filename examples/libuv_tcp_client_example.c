869e4bcef579a969d4865194c1c4f8e8ce2edc5b
swasun <charly.lamothe@sfr.fr>
2018-06-03 22:32:40 +0200

    Fix libuv_tcp_server_example.c, and start libuv_tcp_client_example.c

--- examples/libuv_tcp_client_example.c
+++ examples/libuv_tcp_client_example.c
@@ -1,31 +1,149 @@
+#include <unknownecho/init.h>
+#include <unknownecho/alloc.h>
+#include <unknownecho/bool.h>
+#include <unknownecho/errorHandling/stacktrace.h>
+#include <unknownecho/errorHandling/logger.h>
+#include <unknownecho/console/input.h>
+
 #include <uv.h>
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/*void on_connect(uv_connect_t *req, int status) {
-    if (status < 0) {
-        fprintf(stderr, "connect failed error %s\n", uv_err_name(status));
-        free(req);
+static void on_connect(uv_connect_t *req, int status);
+static void on_write(uv_write_t *req, int status);
+
+typedef struct {
+    uv_write_t req;
+    uv_buf_t buf;
+} write_req_t;
+
+void alloc_cb(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+    /* @todo check returned value */
+    buf->base = (char *)malloc(suggested_size);
+    buf->len = suggested_size;
+}
+
+void free_write_req(uv_write_t *req) {
+    write_req_t *wr = (write_req_t *) req;
+    ue_safe_free(wr->buf.base);
+    ue_safe_free(wr);
+}
+
+void on_write(uv_write_t *req, int status) {
+    if (status) {
+        ue_stacktrace_push_msg("Write error: %s", uv_strerror(status));
+        return;
+    }
+    ue_logger_info("Wrote\n");
+    free_write_req(req);
+}
+
+void on_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
+    ssize_t i;
+
+    if (nread >= 0) {
+        printf(" ! %s\n", buf->base );
+        for (i = 0; i < nread; i++) {
+            printf("%c", buf->base[i]);
+        }
+        printf("\n");
+    }
+    else {
+        uv_close((uv_handle_t *)client, NULL);
+    }
+
+    free(buf->base);
+}
+
+/**
+ * see that: https://luka.strizic.info/post/libuv-standard-input/output-and-TCP-input/output-example/
+ */
+void on_connect(uv_connect_t *connection, int status) {
+    uv_stream_t *stream;
+    write_req_t *req;
+    char *input;
+
+    if (status) {
+        ue_stacktrace_push_msg("Connect error: %s", uv_strerror(status));
         return;
     }
 
-    uv_read_start((uv_stream_t*) req->handle, alloc_buffer, on_read);
-    uv_write()
-    free(req);
-}*/
+    ue_logger_info("Connected");
 
-int main() {
-    /*uv_loop_t *loop;
+    stream = connection->handle;
+
+    /* @todo add error handling */
+    req = (write_req_t *)malloc(sizeof(write_req_t));
+
+    while (true) {
+        if (!(input = ue_input_string(">"))) {
+            ue_logger_error("Invalid input");
+            continue;
+        }
+
+        //if (strcmp(input, "-q") == 0) {
+            //uv_close((uv_handle_t *)stream, NULL);
+//            break;
+  //      }
+
+        /* @todo add error handling for this three functions */
+        req->buf = uv_buf_init(input, strlen(input));
+        uv_write((uv_write_t *)req, stream, &req->buf, 1, on_write);
+        uv_read_start(stream, alloc_cb, on_read);
+    }
+}
+
+void on_walk(uv_handle_t *handle, void *arg) {
+    if (handle) {
+        uv_close(handle, NULL);
+    }
+}
+
+void on_sigint_received(uv_signal_t *handle, int signum) {
+    printf("Signal received: %d\n", signum);
+    if (uv_loop_close(handle->loop) == UV_EBUSY) {
+        uv_walk(handle->loop, on_walk, NULL);
+    }
+}
+
+
+int main(int argc, char **argv) {
+    uv_loop_t *loop;
+    struct sockaddr_in addr;
+    uv_signal_t sigint;
+
+    if (!ue_init()) {
+        fprintf(stderr, "[FATAL] Failed to initialize LibUnknownEcho\n");
+        exit(EXIT_FAILURE);
+    }
 
     loop = uv_default_loop();
 
-    uv_connect_t *connect_req = (uv_connect_t*) malloc(sizeof(uv_connect_t));
-    uv_tcp_t *socket = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
-    uv_tcp_init(loop, socket);
+    /* @todo add error handling */
+    uv_signal_init(loop, &sigint);
 
-    uv_tcp_connect(connect_req, socket, (const struct sockaddr*) res->ai_addr, on_connect);
+    /* @todo add error handling */
+    uv_signal_start(&sigint, on_sigint_received, SIGINT);
 
-    return uv_run(loop, UV_RUN_DEFAULT);*/
+    uv_tcp_t socket;
+    uv_tcp_init(loop, &socket);
+    uv_tcp_keepalive(&socket, 1, 60);
 
-    return 0;
+    uv_ip4_addr("0.0.0.0", 7000, &addr);
+
+    uv_connect_t connect;
+    uv_tcp_connect(&connect, &socket, (const struct sockaddr *) &addr, on_connect);
+
+    uv_run(loop, UV_RUN_DEFAULT);
+
+//clean_up:
+    uv_loop_delete(loop);
+    if (ue_stacktrace_is_filled()) {
+        ue_logger_error("Error(s) occurred with the following stacktrace(s) :");
+        ue_stacktrace_print_all();
+    }
+    ue_uninit();
+    return EXIT_SUCCESS;
 }
--- examples/libuv_tcp_server_example.c
+++ examples/libuv_tcp_server_example.c
@@ -1,3 +1,9 @@
+#include <unknownecho/init.h>
+#include <unknownecho/alloc.h>
+#include <unknownecho/bool.h>
+#include <unknownecho/errorHandling/stacktrace.h>
+#include <unknownecho/errorHandling/logger.h>
+
 #include <uv.h>
 
 #include <stdio.h>
@@ -7,83 +13,203 @@
 #define DEFAULT_PORT 7000
 #define DEFAULT_BACKLOG 128
 
-uv_loop_t *loop;
-
 typedef struct {
     uv_write_t req;
     uv_buf_t buf;
 } write_req_t;
 
+typedef struct {
+    uv_tcp_t **clients;
+    int max_clients;
+    int clients_number;
+} tcp_clients;
+
 void free_write_req(uv_write_t *req) {
     write_req_t *wr = (write_req_t*) req;
-    free(wr->buf.base);
-    free(wr);
+    ue_safe_free(wr->buf.base);
+    ue_safe_free(wr);
 }
 
+/**
+ * @todo check malloc return
+ */
 void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
-    buf->base = (char*) malloc(suggested_size);
+    buf->base = (char *)malloc(suggested_size);
     buf->len = suggested_size;
 }
 
 void on_close(uv_handle_t* handle) {
-    free(handle);
+    ue_safe_free(handle);
 }
 
 void echo_write(uv_write_t *req, int status) {
     if (status) {
-        fprintf(stderr, "Write error %s\n", uv_strerror(status));
+        ue_stacktrace_push_msg("Write error: %s", uv_strerror(status));
     }
     free_write_req(req);
 }
 
 void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
+    write_req_t *req;
+    uv_tcp_t *server;
+    tcp_clients *clients;
+    int id;
+    bool alloc_r;
+
+    alloc_r = false;
+
     if (nread > 0) {
-        write_req_t *req = (write_req_t*) malloc(sizeof(write_req_t));
+        ue_safe_alloc_ret(req, write_req_t, 1, alloc_r);
+        if (!alloc_r) {
+            ue_stacktrace_push_msg("Failed to alloc write_req_t");
+            return;
+        }
+
+        /* @todo add error handling for this two functions */
         req->buf = uv_buf_init(buf->base, nread);
-        uv_write((uv_write_t*) req, client, &req->buf, 1, echo_write);
+        uv_write((uv_write_t *)req, client, &req->buf, 1, echo_write);
+
         return;
     }
+
     if (nread < 0) {
         if (nread != UV_EOF) {
-            fprintf(stderr, "Read error %s\n", uv_err_name(nread));
+            ue_stacktrace_push_msg("Read error: %s", uv_err_name(nread));
         }
-        uv_close((uv_handle_t*) client, on_close);
+        if (!(server = client->loop->data)) {
+            ue_stacktrace_push_msg("User data of loop ptr is null");
+            return;
+        }
+        if (!(clients = server->data)) {
+            ue_stacktrace_push_msg("User data of server ptr is null");
+            return;
+        }
+        id = *(int *)client->data;
+        uv_close((uv_handle_t *) client, on_close);
+        if (id < 0 || id > clients->max_clients) {
+            ue_stacktrace_push_msg("id value isn't valid: '%d' ; client handle is still closed", id);
+            return;
+        }
+        clients->clients[id] = NULL;
+        clients->clients_number--;
     }
 
-    free(buf->base);
+    if (buf->base) {
+        free(buf->base);
+    }
 }
 
 void on_new_connection(uv_stream_t *server, int status) {
+    tcp_clients *clients;
+    uv_tcp_t *client;
+    int id;
+    bool alloc_r;
+
     if (status < 0) {
-        fprintf(stderr, "New connection error %s\n", uv_strerror(status));
+        ue_stacktrace_push_msg("New connection error: %s", uv_strerror(status));
         return;
     }
 
-    uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
-    uv_tcp_init(loop, client);
-    if (uv_accept(server, (uv_stream_t*) client) == 0) {
-        uv_read_start((uv_stream_t*) client, alloc_buffer, echo_read);
+    ue_safe_alloc_ret(client, uv_tcp_t, 1, alloc_r);
+    if (!alloc_r) {
+        ue_stacktrace_push_msg("Failed to alloc write_req_t");
+        return;
     }
-    else {
+
+    /* @todo add error handling */
+    uv_tcp_init(server->loop, client);
+
+    /* @todo add a log to record why the connection isn't accepted by the server */
+    if (uv_accept(server, (uv_stream_t*) client) == 0) {
+        clients = server->data;
+        clients->clients[clients->clients_number] = client;
+        id = clients->clients_number;
+        client->data = &id;
+        clients->clients_number++;
+        /* @todo add error handling here */
+        uv_read_start((uv_stream_t*) client, alloc_buffer, echo_read);
+    } else {
         uv_close((uv_handle_t*) client, on_close);
+    }
+}
+
+void ov_walk(uv_handle_t *handle, void *arg) {
+    if (handle) {
+        uv_close(handle, NULL);
+    }
+}
+
+void on_sigint_received(uv_signal_t *handle, int signum) {
+    printf("Signal received: %d\n", signum);
+    if (uv_loop_close(handle->loop) == UV_EBUSY) {
+        uv_walk(handle->loop, ov_walk, NULL);
     }
 }
 
 int main() {
+    uv_loop_t *loop;
     struct sockaddr_in addr;
+    uv_signal_t sigint;
+    tcp_clients clients;
+    uv_tcp_t server;
+    int i, r;
 
+    if (!ue_init()) {
+        fprintf(stderr, "[FATAL] Failed to initialize LibUnknownEcho\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* @todo add error handling */
     loop = uv_default_loop();
 
-    uv_tcp_t server;
+    /* @todo add error handling */
     uv_tcp_init(loop, &server);
 
+    /* @todo add error handling */
+    uv_signal_init(loop, &sigint);
+
+    /* @todo add error handling */
+    uv_signal_start(&sigint, on_sigint_received, SIGINT);
+
+    /* @todo add error handling */
     uv_ip4_addr("0.0.0.0", DEFAULT_PORT, &addr);
 
+    /* @todo add error handling */
     uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
-    int r = uv_listen((uv_stream_t*) &server, DEFAULT_BACKLOG, on_new_connection);
-    if (r) {
-        fprintf(stderr, "Listen error %s\n", uv_strerror(r));
-        return 1;
+
+    loop->data = &server;
+
+    server.data = &clients;
+    clients.max_clients = DEFAULT_BACKLOG;
+    ue_safe_alloc(clients.clients, uv_tcp_t *, DEFAULT_BACKLOG);
+    for (i = 0; i < clients.max_clients; i++) {
+        clients.clients[i] = NULL;
     }
-    return uv_run(loop, UV_RUN_DEFAULT);
+    clients.clients_number = 0;
+
+    if ((r = uv_listen((uv_stream_t*) &server, DEFAULT_BACKLOG, on_new_connection))) {
+        ue_stacktrace_push_msg("Listen error: %s", uv_strerror(r));
+        goto clean_up;
+    }
+
+    ue_logger_info("Before uv_run()");
+    uv_run(loop, UV_RUN_DEFAULT);
+    ue_logger_info("After uv_run()");
+
+clean_up:
+    if (clients.clients) {
+        for (i = 0; i < clients.max_clients; i++) {
+            if (clients.clients[i]) {
+                free(clients.clients[i]);
+            }
+        }
+        free(clients.clients);
+    }
+    uv_loop_delete(loop);
+    if (ue_stacktrace_is_filled()) {
+        ue_logger_error("Error(s) occurred with the following stacktrace(s) :");
+        ue_stacktrace_print_all();
+    }
+    ue_uninit();
+    return EXIT_SUCCESS;
 }
