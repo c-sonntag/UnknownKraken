96a2fe31f46569d7d79a89051bdb2ecbe26b5564
Charly Lamothe <charly.lamothe@sfr.fr>
2018-07-06 18:05:32 +0200

    Fix copyright in external

--- external/libei.cmake
+++ external/libei.cmake
@@ -1,20 +1,20 @@
  ##########################################################################################
  # Copyright (C) 2018 by Charly Lamothe													  #
  #																						  #
- # This file is part of LibUnknownEchoCryptoModule.										  #
+ # This file is part of LibUnknownEcho.										  			  #
  #																						  #
- #   LibUnknownEchoCryptoModule is free software: you can redistribute it and/or modify   #
+ #   LibUnknownEcho is free software: you can redistribute it and/or modify   			  #
  #   it under the terms of the GNU General Public License as published by				  #
  #   the Free Software Foundation, either version 3 of the License, or					  #
  #   (at your option) any later version.												  #
  #																						  #
- #   LibUnknownEchoCryptoModule is distributed in the hope that it will be useful,        #
+ #   LibUnknownEcho is distributed in the hope that it will be useful,        			  #
  #   but WITHOUT ANY WARRANTY; without even the implied warranty of						  #
  #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						  #
  #   GNU General Public License for more details.										  #
  #																						  #
  #   You should have received a copy of the GNU General Public License					  #
- #   along with LibUnknownEchoCryptoModule.  If not, see <http://www.gnu.org/licenses/>.  #
+ #   along with LibUnknownEcho.  If not, see <http://www.gnu.org/licenses/>.  			  #
  ##########################################################################################
 
 add_custom_target(ei)
